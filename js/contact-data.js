b614075702c80acdc1fa2f195505bd83b9d70620
Andy <ydna.nonhoj@hotmail.com>
2019-07-07 21:05:16 -0700

    My attempt at the 14Four skills assessment.

---
+++ js/contact-data.js
@@ -0,0 +1,134 @@
+var contactData = [
+    {
+        name: "Andy",
+        phone: "555-999-0000",
+        email: "andy@email.com",
+        status: "online",
+        address: {
+            street: "43 Over There St",
+            city: "Vancouver",
+            state: "WA",
+            zip: "98661"
+        }
+    },
+    {
+        name: "Optimus",
+        phone: "555-112-2222",
+        email: "optimus@cybertron.com",
+        status: "online",
+        address: {
+            street: "Semi Truck Way",
+            city: "Cybertron",
+            state: "Planet",
+            zip: "100101010"
+        }
+    },
+    {
+        name: "Jablinski",
+        phone: "555-231-5244",
+        email: "jablinkski@jablinkskigames.com",
+        status: "offline",
+        address: {
+            street: "439 Tenacious Ave",
+            city: "Hollywood",
+            state: "CA",
+            zip: "90210"
+        }
+    },
+    {
+        name: "Patrick",
+        phone: "555-321-1123",
+        email: "patrick.star@aol.com",
+        status: "away",
+        address: {
+            street: "120 Conch St",
+            city: "BK Bottom",
+            state: "OCE",
+            zip: "44332"
+        }
+    },
+    {
+        name: "Ash",
+        phone: "555-123-9988",
+        email: "ash@verybest.com",
+        status: "online",
+        address: {
+            street: "75 Oak Street",
+            city: "Pallet Town",
+            state: "Kanto",
+            zip: "54434"
+        }
+    },
+    {
+        name: "Cathie",
+        phone: "555-223-9888",
+        email: "campbellsoup@comcast.net",
+        status: "away",
+        address: {
+            street: "983 Burnside St",
+            city: "Portland",
+            state: "OR",
+            zip: "98652"
+        }
+    },
+    {
+        name: "Swoop",
+        phone: "555-655-3454",
+        email: "swoop@ewu.edu",
+        status: "offline",
+        address: {
+            street: "526 5th St",
+            city: "Cheney",
+            state: "WA",
+            zip: "99004"
+        }
+    },
+    {
+        name: "Alexis",
+        phone: "555-888-2333",
+        email: "NotaCIAagent@secret.gov",
+        status: "away",
+        address: {
+            street: "4493 Toad Way",
+            city: "Albany",
+            state: "NY",
+            zip: "12212"
+        }
+    },
+    {
+        name: "Benji",
+        phone: "555-652-5664",
+        email: "squareplate39@yahoo.com",
+        status: "away",
+        address: {
+            street: "543 Ridgeview Ct",
+            city: "Tamba Bay",
+            state: "FL",
+            zip: "33601"
+        }
+    },
+    {
+        name: "Trouble Maker",
+        phone: "555-766-5664",
+        email: "troublemaker@superduperlongemailaddress.com",
+        status: "away",
+        address: {
+            street: "123 Street St",
+            city: "Thousand Oaks",
+            state: "CA",
+            zip: "90154"
+        }
+    },
+    {
+        name: "Nathan",
+        phone: "555-711-5645",
+        email: "natedoggeh@gmail.com",
+        status: "online",
+        address: {
+            street: "5429 Balloon Stand Blvd",
+            city: "Houston",
+            state: "TX",
+            zip: "49129"
+        }
+    }
+];
---
+++ js/contact-list.js
@@ -0,0 +1,136 @@
+var contacts = document.getElementById("contacts");
+var select = document.getElementById("infoSelect");
+
+buildContacts(contacts);
+
+function buildContacts(contacts) {
+    // contactData can be found in js/contact-data.js
+    contactData.forEach(function (contact, index) {
+        fillContact(contacts, contact, index);
+    });
+}
+
+function fillContact(contacts, contact, rowIndex) {
+    var nameElement = getNameElement(contact, rowIndex);
+    var infoElement = getInfoElement(contact);
+    var underlyingElement = getUnderlyingElement(contact);
+    var contactElement = getContactElement(nameElement, underlyingElement, infoElement, rowIndex);
+
+    contacts.appendChild(contactElement);
+}
+
+function getContactElement(nameElement, underlyingElement, infoElement, rowIndex) {
+    var contactElement = document.createElement("tr");
+
+    contactElement.appendChild(nameElement);
+    contactElement.appendChild(underlyingElement);
+    contactElement.appendChild(infoElement);
+
+    return contactElement;
+}
+
+
+function getNameElement(contact, rowIndex) {
+    var nameCell = document.createElement("td");
+    nameCell.setAttribute("class", "contact-name");
+    nameCell.setAttribute("id", rowIndex);
+    nameCell.setAttribute("onmouseover", "mouseOverName(" + rowIndex + ")");
+    nameCell.setAttribute("onmouseout", "mouseOutName()");
+
+    var nameElement = document.createElement("span");
+    var statusCircle = getStatusCircle(contact.status);
+
+    nameElement.appendChild(document.createTextNode(contact.name));
+    statusCircle.appendChild(nameElement);
+    nameCell.appendChild(statusCircle);
+
+    return nameCell;
+}
+
+function getInfoElement(contact) {
+    var infoElement = (select.value == 1)
+        ? buildInfoCell(contact.phone)
+        : buildInfoCell(contact.email);
+
+    return infoElement;
+}
+
+function getUnderlyingElement(contact) {
+    var underlyingData = document.createElement("td");
+    underlyingData.setAttribute("class", "underlying");
+
+    var emailLink = document.createElement("a");
+    emailLink.setAttribute("class", "email");
+    emailLink.setAttribute("href", contact.email)
+    emailLink.appendChild(document.createTextNode(contact.email));
+
+    var phone = document.createElement("p");
+    phone.appendChild(document.createTextNode(contact.phone.replace(/-/g, '.')));
+
+    var address = document.createElement("p");
+    address.appendChild(document.createTextNode(contact.address.street));
+
+    var lineBreak = document.createElement("br");
+    var textInfo = contact.address.city + " " + contact.address.state + ", " + contact.address.zip;
+
+    address.appendChild(lineBreak);
+    address.appendChild(document.createTextNode(textInfo));
+
+    underlyingData.appendChild(emailLink);
+    underlyingData.appendChild(phone);
+    underlyingData.appendChild(address);
+
+    return underlyingData;
+}
+
+function buildInfoCell(text) {
+    var cellElement = document.createElement("td");
+    cellElement.textContent = text;
+    cellElement.setAttribute("class", "contact-info")
+
+    return cellElement;
+}
+
+function getStatusCircle(status) {
+    var canvas = document.createElement("div");
+    canvas.setAttribute("class", "contact-" + status);
+
+    return canvas;
+}
+
+function clearContacts() {
+    var contacts = document.getElementById("contacts");
+    contacts.textContent = "";
+}
+
+function updateContactList() {
+    clearContacts();
+    buildContacts(contacts);
+}
+
+function mouseOverName(id) {
+    var cells = mergeNameAndInfoCellsToArray();
+    Array.prototype.forEach.call(cells, function (cell) {
+        if (cell.id != id) {
+            cell.style.opacity = ".25";
+        }
+    });
+}
+
+function mouseOutName() {
+    var cells = mergeNameAndInfoCellsToArray();
+    Array.prototype.forEach.call(cells, function (cell) {
+        cell.style.opacity = "1";
+    });
+}
+
+function mergeNameAndInfoCellsToArray() {
+    var nameCells = document.getElementsByClassName("contact-name");
+    var infoCells = document.getElementsByClassName("contact-info");
+
+    var cells = Array.prototype.slice.call(nameCells).concat(
+        Array.prototype.slice.call(infoCells)
+    );
+
+    return cells;
+}
